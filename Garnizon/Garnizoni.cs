a240272a9e9215745e9cf1c96b39d7a8befa18ad
bezanoviic <bezanovicbranko@gmail.com>
2024-05-16 20:57:13 +0200

    first commit

---
+++ Garnizon/Garnizoni.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Garnizon
+{
+    internal class Garnizoni : INotifyPropertyChanged
+    {
+        private string naziv;
+        private int id;
+        private string adresa;
+        private string ikonica;
+        private List<Jedinica> jedinice;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string v)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(v));
+        }
+
+        public Garnizoni()
+        {
+            this.naziv = "";
+            this.id = 0;
+            this.adresa = "";
+            this.ikonica = "";
+            this.jedinice = new List<Jedinica>();
+        }
+
+        public Garnizoni(string naziv,string id,string adresa,string ikonica)
+        {
+            this.naziv = naziv;
+            this.id = 0;
+            this.adresa = adresa;
+            this.ikonica = ikonica;
+            this.jedinice = new List<Jedinica>();
+        }
+
+        public string Naziv
+        {
+            get { return this.naziv; }
+            set
+            {
+                if (this.naziv != value)
+                {
+                    this.naziv = value;
+                    this.NotifyPropertyChanged("Naziv");
+                }
+            }
+        }
+
+        public int ID
+        {
+            get { return this.id; }
+            set
+            {
+                if(this.id!=value)
+                {
+                    this.id = value;
+                    this.NotifyPropertyChanged("ID");
+                }
+            }
+        }
+
+        public string Adresa
+        {
+            get { return this.adresa; }
+            set
+            {
+                if (this.adresa != value)
+                {
+                    this.adresa = value;
+                    this.NotifyPropertyChanged("Adresa");
+                }
+            }
+        }
+
+        public string Ikonica
+        {
+            get { return this.ikonica; }
+            set
+            {
+                if (this.ikonica != value)
+                {
+                    this.ikonica = value;
+                    this.NotifyPropertyChanged("Ikonica");
+                }
+            }
+        }
+
+        public List<Jedinica> Jedinice
+        {
+            get { return this.jedinice; }
+            set
+            {
+                if(this.jedinice!=value)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Garnizon/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Garnizon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Garnizoni> garnizoni = new ObservableCollection<Garnizoni>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Garnizoni g1 = new Garnizoni("Avalski Štit", "1", "Mileta Kitica 77", "D:/Faks/OOT/Projekat/Slike/amblem-vojnobezbednosne-agencije.png");
+            Jedinica j1 = new Jedinica("1.Pesadija", "Mileta Kitica 77", "");
+            g1.Jedinice.Add(j1);
+            garnizoni.Add(g1);
+            Garnizoni.ItemsSource = garnizoni;
+        }
+
+        private void Jedinice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Garnizoni_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Garnizoni g = Garnizoni.SelectedItem as Garnizoni;
+            Ikonica1.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(g.Ikonica);
+        }
+
+        private void GarnizoniRaspored1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void GarnizoniRaspored2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void JediniceRaspored1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void JediniceRaspored1_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void JediniceRaspored1_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void JediniceRaspored2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void JediniceRaspored2_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void JediniceRaspored2_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
